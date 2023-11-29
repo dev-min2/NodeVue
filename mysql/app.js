e86a9dd59e6c060e902b84936433e35697549042
Kmin <alsry156@naver.com>
2023-11-29 12:41:46 +0900

    k

--- mysql/app.js
+++ mysql/app.js
@@ -4,3 +4,11 @@ const app = express();
 app.use(express.json( {
     limit : '50mb'
 }));
+
+app.listen(3000, () => {
+    console.log('Server start');
+})
+
+app.get('/customerList', (req, res) => {
+    res.send('목록 조회');
+})
